2ee3dabef1103e1fe582eda7c41c8482415918cc
Rastark <alx.aurora@hotmail.it>
2016-06-12 21:42:15 +0200

    added AdminController

--- src/main/java/it/uniroma3/controller/AdminController.java
+++ src/main/java/it/uniroma3/controller/AdminController.java
@@ -1,5 +1,28 @@
 package it.uniroma3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import it.uniroma3.model.Admin;
+import it.uniroma3.service.AdminService;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	AdminService adminService;
+	
+	@ModelAttribute("admin")
+	public Admin createAdmin() {
+		return new Admin();
+	}
+	
+	@RequestMapping("/listadmin")
+	public String listAdmin(Model model) {
+		model.addAttribute("adminList", adminService.listAdmin());
+		return "admin";
+	}	
 }
